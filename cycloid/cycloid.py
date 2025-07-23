37369c37e763b815a3ac71a74376be7c43ceb27d
Miguel Flores-Acton <floresacton@gmail.com>
2025-07-23 19:10:55 -0400

    Finish cycloid

--- cycloid/cycloid.py
+++ cycloid/cycloid.py
@@ -2,6 +2,32 @@ import Part
 import FreeCAD as App
 import math
 
+def spread(input, curvature):
+    x = input * 2 - 1
+    mapped = (x - curvature * x) / (curvature - 2 * curvature * abs(x) + 1)
+    return (mapped + 1) / 2
+
+def cycloid_point(theta, teethp1, eccentricity, teethp1_ecc, outer_radius, outer_pin_radius):
+    sin_theta = math.sin(theta)
+    cos_theta = math.cos(theta)
+    
+    teethp1_theta = teethp1 * theta
+    sin_teethp1_theta = math.sin(teethp1_theta)
+    cos_teethp1_theta = math.cos(teethp1_theta)
+
+    x = outer_radius * sin_theta + eccentricity * sin_teethp1_theta
+    y = outer_radius * cos_theta + eccentricity * cos_teethp1_theta
+
+    dx = outer_radius * cos_theta + teethp1_ecc * cos_teethp1_theta
+    dy = -outer_radius * sin_theta - teethp1_ecc * sin_teethp1_theta
+
+    length = math.hypot(dx, dy)
+
+    px = x + dy / length * outer_pin_radius
+    py = y - dx / length * outer_pin_radius
+
+    return (px, py)
+
 class Cycloid:
     def __init__(self, obj):
         obj.Proxy = self
@@ -10,69 +36,59 @@ class Cycloid:
         obj.addProperty("App::PropertyLength","OuterDiameter").OuterDiameter = 52
         obj.addProperty("App::PropertyLength","OuterPinDiameter").OuterPinDiameter = 4
         obj.addProperty("App::PropertyLength","Height").Height = 4
-        
-        self.points_per_tooth = 50
-        self.curvature = 0.65
+        obj.addProperty("App::PropertyQuantity","PointsPerTooth").PointsPerTooth = 200
 
     def execute(self, obj):
-        spline = Part.BSplineCurve()
-        spline.interpolate(self.cycloid_points(obj))
-
-        edge = spline.toShape()
-        wire = Part.Wire(edge)
-        face = Part.Face(wire)
-
-        height = float(obj.Height)
-        prism = face.extrude(App.Vector(0, 0, height / 2))
-        neg_prism = face.extrude(App.Vector(0, 0, -height / 2))
-     
-        obj.Shape = prism.fuse(neg_prism)
-        
-    def cycloid_points(self, obj):
         teeth = int(obj.Teeth)
         eccentricity = float(obj.Eccentricity)
         outer_diameter = float(obj.OuterDiameter)
         outer_pin_diameter = float(obj.OuterPinDiameter)
-        points_per_tooth = 50
+        height = float(obj.Height)
+        points_per_tooth = int(obj.PointsPerTooth)
         
-        teeth_plus_one = teeth + 1
+        tooth_edge = self.tooth_edge(teeth, eccentricity, outer_diameter, outer_pin_diameter, points_per_tooth)
+        
+        center = App.Vector(0, 0, 0)
+        axis = App.Vector(0, 0, 1)
+        tooth_angle = 360/teeth
+        
+        edges = [tooth_edge]
+        for t in range(1, teeth):
+            edge_copy = tooth_edge.copy()
+            edge_copy.rotate(center, axis, t*tooth_angle)
+            edges.append(edge_copy)
+            
+        wire = Part.Wire(edges)
+        wire.translate(App.Vector(0,0,-height/2))
+        face = Part.Face(wire)
+     
+        obj.Shape = face.extrude(App.Vector(0, 0, height))
+        
+    def tooth_edge(self, teeth, eccentricity, outer_diameter, outer_pin_diameter, points_per_tooth):
+        teethp1 = teeth + 1
+        teethp1_ecc = eccentricity * teethp1
+        
         outer_pin_radius = outer_pin_diameter / 2
         outer_radius = outer_diameter / 2
 
         radians_per_tooth = 2 * math.pi / teeth
 
-        profile_points = []
-
-        for tooth_index in range(teeth):
-            for point_index in range(points_per_tooth):
-                tooth_fraction = point_index / points_per_tooth
-                theta = (tooth_index + self.spread_map(tooth_fraction)) * radians_per_tooth
-
-            x = (outer_radius * math.sin(theta) + eccentricity * math.sin(teeth_plus_one * theta))
-            y = (outer_radius * math.cos(theta) + eccentricity * math.cos(teeth_plus_one * theta))
-
-            dx = outer_radius * math.cos(theta) + eccentricity * teeth_plus_one * math.cos(teeth_plus_one * theta)
-            dy = -outer_radius * math.sin(theta) - eccentricity * teeth_plus_one * math.sin(teeth_plus_one * theta)
-
-            tangent_length = math.sqrt(dx * dx + dy * dy)
-
-            px = x + dy / tangent_length * outer_pin_radius
-            py = y - dx / tangent_length * outer_pin_radius
-
-            profile_points.append(App.Vector(px, py, 0))
-
-        profile_points.append(profile_points[0])
-        return profile_points
+        points = [None] * (points_per_tooth + 1)
         
-    def spread_map(self, input):
-        x = input * 2 - 1
-        mapped = (x - self.curvature * x) / (self.curvature - 2 * self.curvature * abs(x) + 1)
-        return (mapped + 1) / 2
+        for p in range(points_per_tooth+1):
+            theta = spread(p / points_per_tooth, 0.65) * radians_per_tooth
+            
+            x, y = cycloid_point(theta, teethp1, eccentricity, teethp1_ecc, outer_radius, outer_pin_radius)
+            
+            points[p] = App.Vector(x, y, 0)
+        
+        spline = Part.BSplineCurve()
+        spline.interpolate(points)
+
+        return spline.toShape()
 
 def make_cycloid():
     obj = App.ActiveDocument.addObject("Part::FeaturePython","Cycloid")
     Cycloid(obj)
     obj.ViewObject.Proxy = 0
     App.ActiveDocument.recompute()
-
-make_cycloid()
